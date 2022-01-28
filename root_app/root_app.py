2b1fda51213413c9b2365ec28b8ce696f6179108
Vlastik <vlastikczech1@gmail.com>
2022-01-27 23:56:24 -0700

    update root_app

--- root_app/root_app.py
+++ root_app/root_app.py
@@ -2,10 +2,12 @@ import logging
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+base_path = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(base_path / "templates"))
 
 logging.basicConfig(filename='/var/log/bento/root_app.log',level=logging.DEBUG)
 log = logging.getLogger(__name__)
